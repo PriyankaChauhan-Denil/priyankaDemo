3bd2bd046d377f5fcc411b2c1e0750f39242b7b9
PriyankaChauhan-Denil <priyankachauhan.rnw@gmail.com>
2024-11-07 14:01:48 +0530

    commited!

---
+++ src/priyanka/p1.java
@@ -0,0 +1,24 @@
+package priyanka;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class p1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/client");
+		
+		driver.findElement(By.id("userEmail")).sendKeys("pdc12345@gmail.com");
+		driver.findElement(By.id("userPassword")).sendKeys("Pdc12345");
+		driver.findElement(By.id("login")).click();
+		Thread.sleep(3000);
+	}
+
+}				
